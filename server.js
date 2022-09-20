05fea51cd8757bd2d90cf7cf771c19107425294f
Sohaib Ali <saybe.ali0618@gmail.com>
2022-09-20 19:45:39 -0400

    fixed port

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const mongoose = require('mongoose');
 
 const express = require("express")
 const axios = require('axios')
-const { MONGODB_URI} = process.env;
-const PORT = 4000;
+const { MONGODB_URI, PORT} = process.env || 4000;
+// const PORT = 4000;
 
 const animeControl = require('./controllers/anime-controller')
 //const authController = require('./controllers/auth')
