3e24c4756ddbfd47ebcaa618aeb583b48bfbcb3f
Sohaib Ali <saybe.ali0618@gmail.com>
2022-09-21 10:28:20 -0400

    added new data key-values

--- models/Anime.js
+++ models/Anime.js
@@ -13,9 +13,28 @@ const reviewSchema = new mongoose.Schema ({
 const AnimeSchema = new mongoose.Schema({
  mal_id: Number,
 url: String,
+status: String,
+images: {
+    jpg: {
+        image_url: String,
+        small_image_url: String,
+        large_image_url: String
+    },
+    webp: {
+        image_url: String,
+        small_image_url: String,
+        large_image_url: String
+    }
+},
+trailer: {
+    embed_url: String
+},
 title: String,
+title_japanese: String,
+rating: String,
 episodes: Number,
 synopsis: String,
+
 reviews: [reviewSchema]
       });
 
